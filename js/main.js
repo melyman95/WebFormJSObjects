32c32297695f97b9b8dbbcc4c929666195d011d0
melyman95 <mattlyman12@gmail.com>
2021-05-11 18:06:59 -0700

    added validation

--- js/main.js
+++ js/main.js
@@ -9,25 +9,71 @@ window.onload = function () {
     addBtn.onclick = addVehicle;
 };
 function addVehicle() {
+    clearInputErrors();
     if (AllDataValid()) {
         getVehicle();
         displayVehicle(myVehicle);
     }
 }
+function getInputById(id) {
+    return document.getElementById(id);
+}
 function AllDataValid() {
-    return true;
+    var isValid = true;
+    var make = getInputById("make").value;
+    var model = getInputById("model").value;
+    var year = getInputById("year").value;
+    var yearValue = parseInt(year);
+    var price = getInputById("cost").value;
+    var priceValue = parseFloat(price);
+    if (make == "") {
+        isValid = false;
+        var errorSummary = document.getElementById("validation-summary");
+        var errorItem = document.createElement("li");
+        errorItem.innerText = "Make is required.";
+        errorSummary.appendChild(errorItem);
+    }
+    if (model == "") {
+        isValid = false;
+        var errorSummary = document.getElementById("validation-summary");
+        var errorItem = document.createElement("li");
+        errorItem.innerText = "Model is required.";
+        errorSummary.appendChild(errorItem);
+    }
+    if (year == "" || isNaN(yearValue) || year.length != 4) {
+        isValid = false;
+        var errorSummary = document.getElementById("validation-summary");
+        var errorItem = document.createElement("li");
+        errorItem.innerText = "Year field is empty or is invalid.";
+        errorSummary.appendChild(errorItem);
+    }
+    if (price == "" || isNaN(priceValue)) {
+        isValid = false;
+        var errorSummary = document.getElementById("validation-summary");
+        var errorItem = document.createElement("li");
+        errorItem.innerText = "Price field is empty or isn't a number.";
+        errorSummary.appendChild(errorItem);
+    }
+    return isValid;
+}
+function clearInputErrors() {
+    var errorSummary = document.getElementById("validation-summary");
+    errorSummary.innerHTML = "";
 }
 function displayVehicle(vehicle) {
     var displayDiv = document.getElementById("display");
     var vehicleMake = document.createElement("h2");
-    vehicleMake.innerHTML = myVehicle.make;
+    vehicleMake.innerHTML = "Make: " + myVehicle.make;
     displayDiv.appendChild(vehicleMake);
     var vehicleModel = document.createElement("p");
-    vehicleModel.innerHTML = myVehicle.model;
+    vehicleModel.innerHTML = "Model: " + myVehicle.model;
     displayDiv.appendChild(vehicleModel);
     var vehicleYear = document.createElement("p");
-    vehicleYear.innerHTML = myVehicle.year;
+    vehicleYear.innerHTML = "Year: " + myVehicle.year;
     displayDiv.appendChild(vehicleYear);
+    var vehiclePrice = document.createElement("p");
+    vehiclePrice.innerHTML = "$" + myVehicle.price;
+    displayDiv.appendChild(vehiclePrice);
 }
 function getVehicle() {
     var vehicle = myVehicle;
@@ -37,5 +83,7 @@ function getVehicle() {
     vehicle.model = modelInput.value;
     var yearInput = document.getElementById("year");
     vehicle.year = yearInput.value;
+    var priceInput = document.getElementById("cost");
+    vehicle.price = priceInput.value;
     return vehicle;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -3,6 +3,7 @@ class Vehicle {
     make:string;
     model:string;
     year:string;
+    price:string;
 }
 
 let myVehicle = new Vehicle();
@@ -13,34 +14,97 @@ window.onload = function () {
 }
 
 function addVehicle() {
-    
+    clearInputErrors();
     if(AllDataValid()) {
         getVehicle();
         displayVehicle(myVehicle);
     }
 }
 
+function getInputById(id:string):HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
+}
+
 function AllDataValid() {
-    return true;
+    let isValid = true;
+    
+    let make = getInputById("make").value;
+
+    let model = getInputById("model").value;
+
+    let year = getInputById("year").value;
+    let yearValue = parseInt(year);
+
+    let price = getInputById("cost").value;
+    let priceValue = parseFloat(price);
+
+    if(make == "") {
+        isValid = false;
+        let errorSummary = document.getElementById("validation-summary");
+        let errorItem = document.createElement("li");
+        errorItem.innerText = "Make is required.";
+
+        errorSummary.appendChild(errorItem);
+    }
+
+    if(model == "") {
+        isValid = false;
+        let errorSummary = document.getElementById("validation-summary");
+        let errorItem = document.createElement("li");
+        errorItem.innerText = "Model is required.";
+
+        errorSummary.appendChild(errorItem);
+    }
+
+    if(year == "" || isNaN(yearValue) || year.length != 4) {
+        isValid = false;
+        let errorSummary = document.getElementById("validation-summary");
+        let errorItem = document.createElement("li");
+        errorItem.innerText = "Year field is empty or is invalid.";
+
+        errorSummary.appendChild(errorItem);
+    }
+
+    if(price == "" || isNaN(priceValue)) {
+        isValid = false;
+        let errorSummary = document.getElementById("validation-summary");
+        let errorItem = document.createElement("li");
+        errorItem.innerText = "Price field is empty or isn't a number.";
+
+        errorSummary.appendChild(errorItem);
+    }
+
+    return isValid;
+}
+
+function clearInputErrors() {
+    let errorSummary = document.getElementById("validation-summary");
+    errorSummary.innerHTML = "";
 }
 
 function displayVehicle(vehicle:Vehicle):void {
     let displayDiv = <HTMLElement>document.getElementById("display");
     
     let vehicleMake = document.createElement("h2");
-    vehicleMake.innerHTML =  myVehicle.make;
+    vehicleMake.innerHTML =  "Make: " + myVehicle.make;
     
     displayDiv.appendChild(vehicleMake);
 
     let vehicleModel = document.createElement("p");
-    vehicleModel.innerHTML = myVehicle.model;
+    vehicleModel.innerHTML = "Model: " + myVehicle.model;
 
     displayDiv.appendChild(vehicleModel);
 
     let vehicleYear = document.createElement("p");
-    vehicleYear.innerHTML = myVehicle.year;
+    vehicleYear.innerHTML = "Year: " + myVehicle.year;
 
     displayDiv.appendChild(vehicleYear);
+
+    let vehiclePrice = document.createElement("p");
+    vehiclePrice.innerHTML = "$" + myVehicle.price;
+
+    displayDiv.appendChild(vehiclePrice);
+
 }
 
 function getVehicle(){
@@ -54,6 +118,9 @@ function getVehicle(){
 
     let yearInput = <HTMLInputElement>document.getElementById("year");
     vehicle.year = yearInput.value;
+
+    let priceInput = <HTMLInputElement>document.getElementById("cost");
+    vehicle.price = priceInput.value;
 
     return vehicle;
 }
